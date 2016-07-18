b189e7baf7c619bb36221f3be7363ef357b5e4dd
ismailrei <ismailrei@gmail.com>
2016-07-18 18:13:54 +0100

    commit

--- emailing/index.js
+++ emailing/index.js
@@ -6,3 +6,26 @@ var mandrillClient=new mandrill.Mandrill("ZraZpvNS5AbqnzqZnSmh2g");
 },function(e){
     console.log(e);
 });*/
+var plugin=function(options){
+    var seneca=this;
+    seneca.add({area:"email",action:"send"},function(args,done)
+    {
+        console.log(args);
+        var message={
+            "html":args.content,
+            "subject":args.subject,
+            "to":[{
+                "email":args.to,
+                "name":args.name,
+                "type":"to"
+            }],
+            "from_email":"ismailrei@gmail.com",
+            "from_name":"ismailrei"
+        }
+        mandrillClient.messages.send({"message":message},
+        function(result){
+            done(null,{status:result.status});
+        },function(d)
+        )
+    })
+}
